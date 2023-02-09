d252723009d2b60b01768a048ac871f41a901120
Diaz Melisa <melisaa.diazzz06@gmail.com>
2023-02-09 17:40:13 -0500

    Commit inicial

---
+++ ejercicios/ejercicio 1.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    nombre = input("Ingresa tu nombre: ")
+    edad = int(input("Ingresar edad: "))
+
+    print(f"¡Hola {nombre}! Tú tienes {edad} años.")
---
+++ ejercicios/ejercicio 2.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    import math
+
+    print("Ingrese el radio del circulo")
+    r = float(input())
+    a = math.pi * (r * r)
+    print("El area del circulo con radio", r, "es:", a)
